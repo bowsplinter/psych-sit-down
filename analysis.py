f4b4bac0e7680c4a58189f0184df6ebcdfb982c9
Vivek Kalyan <vivekkalyansk@gmail.com>
2018-04-16 11:43:15 +0800

    Create project with data

---
+++ analysis.py
@@ -0,0 +1,8 @@
+control_group_time = [1148,1084,1480,1474,1462,1455,1435,1385,1380,1363]
+control_group_score = [3,4,1,1,0,5,2,1,2,2]
+
+experiment_group_time = [600,790,1020,713,770,1279,668,932,1102,806,735]
+experiment_group_score = [1,4,0,2,2,2,1,1,1,0,2]
+
+if __name__ == '__main__':
+    main()
